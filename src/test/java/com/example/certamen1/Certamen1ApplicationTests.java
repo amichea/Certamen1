582bc8878647f8e8d4f1151cbd565c6a83d3730b
Ameli <amichea@usm.cl>
2026-04-24 15:14:30 -0400

    first commit

---
+++ src/test/java/com/example/certamen1/Certamen1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.certamen1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Certamen1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
